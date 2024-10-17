049d95b173798700ddb12cb35a80e667dd4d9bd8
Noel Paing Oak Soe <u6530183@au.edu>
2024-10-17 17:21:14 +0700

    removed middleware, add token validatoin in endpoints

--- app/api/event/create/route.js
+++ app/api/event/create/route.js
@@ -1,7 +1,31 @@
 import Event from '@/models/Event.js'
 import { NextRequest, NextResponse } from 'next/server'
 
+import jwt from 'jsonwebtoken'
+
+const secret = process.env.JWT_SECRET
+
+const verifyToken = (req) => {
+  const token = req.headers.get('Authorization')?.split(' ')[1]
+  console.log('token', token)
+  if (!token) {
+    return false
+  }
+  try {
+    return jwt.verify(token, secret)
+  } catch (err) {
+    return false
+  }
+}
+
 export async function POST(req) {
+  const decoded = verifyToken(req)
+  if (!decoded) {
+    return NextResponse.json({
+      success: false,
+      message: 'Unauthorized',
+    })
+  }
   const {
     eventImageUrl,
     name,
