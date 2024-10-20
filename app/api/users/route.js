8b697dcacc6e8fc58b3f6792641e1ccaf7ad2010
HtetAungShine6 <htetaungshine211299@gmail.com>
2024-10-20 23:00:47 +0700

    updated get all users

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,16 +1,15 @@
 import User from '@/models/User.js'
 import e from 'cors'
 import { NextResponse } from 'next/server'
-export async function GET(req) {
-  try {
-    const users = await User.find().select('-password')
 
-    const response = {
+export async function GET() {
+  try {
+    const users = await User.find().select('-password') 
+
+    return NextResponse.json({
       success: true,
       message: users,
-    }
-
-    return NextResponse.json({ success: true, message: users })
+    })
   } catch (error) {
     return NextResponse.json(error)
   }
