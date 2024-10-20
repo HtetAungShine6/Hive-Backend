a2ccb5c752aa03588d6427ce1f0e1d8edb9b7ab6
HtetAungShine6 <htetaungshine211299@gmail.com>
2024-10-20 23:13:25 +0700

    Resolved Conflicts

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,16 +1,49 @@
-import User from '@/models/User.js'
-import e from 'cors'
+import User from '@/models/User'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
   try {
     const users = await User.find().select('-password') 
 
+    const formattedUsers = users.map(user => {
+      const formattedDateOfBirth = user.dateOfBirth
+        ? user.dateOfBirth.toISOString().split('T')[0] 
+        : null;
+
+      return {
+        ...user.toObject(),
+        dateOfBirth: formattedDateOfBirth, 
+      };
+    });
+
     return NextResponse.json({
       success: true,
-      message: users,
-    })
+      message: formattedUsers,
+    });
   } catch (error) {
-    return NextResponse.json(error)
+    return NextResponse.json({
+      success: false,
+      message: 'Error retrieving users',
+    });
   }
 }
+
+
+// import User from '@/models/User'
+// import { NextResponse } from 'next/server'
+
+// export async function GET() {
+//   try {
+//     const users = await User.find().select('-password') // Exclude password field
+
+//     return NextResponse.json({
+//       success: true,
+//       message: users,
+//     })
+//   } catch (error) {
+//     return NextResponse.json({
+//       success: false,
+//       message: 'Error retrieving users',
+//     })
+//   }
+// }
