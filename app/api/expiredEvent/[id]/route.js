6b7f14b14a2c7cfc357fa195e4e853376b0a0258
HtetAungShine6 <htetaungshine211299@gmail.com>
2024-10-29 15:35:01 +0700

    added get expired event by id

---
+++ app/api/expiredEvent/[id]/route.js
@@ -0,0 +1,76 @@
+import ExpiredEvent from '@/models/ExpiredEvent'
+import User from '@/models/User'
+import { NextResponse } from 'next/server'
+
+export async function GET(req, { params }) {
+  const { id } = params
+  try {
+    const event = await ExpiredEvent.findById(id)
+
+    if (!event) {
+      return NextResponse.json({ success: false, message: 'Expired Event not found' })
+    }
+
+    // Format the startDate and endDate
+    const formattedStartDate = event.startDate
+      ? event.startDate.toISOString().split('T')[0]
+      : null;
+
+    const formattedEndDate = event.endDate
+      ? event.endDate.toISOString().split('T')[0]
+      : null;
+
+    // Format the startTime and endTime (if you want to include them as well)
+    const formattedStartTime = event.startTime
+      ? event.startTime.toISOString().split('T')[1].split(':')[0] + ':' + event.startTime.toISOString().split('T')[1].split(':')[1]
+      : null;
+
+    const formattedEndTime = event.endTime
+      ? event.endTime.toISOString().split('T')[1].split(':')[0] + ':' + event.endTime.toISOString().split('T')[1].split(':')[1]
+      : null;
+
+      const updatedParticipants = await Promise.all(
+        event.participants.map(async participant => {
+          const user = await User.findById(participant.userid)
+          if (user) {
+            return {
+              userid: user._id,
+              name: user.name,
+              profileImageUrl: user.profileImageUrl,
+              bio: user.bio,
+            }
+          }
+          // If user not found, return the existing participant data
+          return participant
+        })
+      )
+
+      const updatedOrganizer = await User.findById(event.organizer)
+        const organizerDetails = updatedOrganizer
+          ? {
+              userid: updatedOrganizer._id,
+              name: updatedOrganizer.name,
+              profileImageUrl: updatedOrganizer.profileImageUrl,
+              bio: updatedOrganizer.bio,
+            }
+          : event.organizer
+
+    return NextResponse.json({
+      success: true,
+      message: {
+        ...event.toObject(),
+        startDate: formattedStartDate,
+        endDate: formattedEndDate,
+        startTime: formattedStartTime,
+        endTime: formattedEndTime,
+        participants: updatedParticipants,
+        organizer: organizerDetails,
+      },
+    }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: 'Error retrieving event',
+    }, { status: 500 })
+  }
+}
