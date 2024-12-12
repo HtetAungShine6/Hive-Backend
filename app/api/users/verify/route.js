3e3e9143e94c4706d6a3bca5ed31697c70f8a5a5
Noel Paing Oak Soe <u6530183@au.edu>
2024-12-12 21:25:28 +0700

    admin signin endpoint, verify reject user endpoint

---
+++ app/api/users/verify/route.js
@@ -0,0 +1,31 @@
+import User from '@/models/User'
+import { NextResponse } from 'next/server'
+
+export async function PUT(req) {
+  const { userid } = await req.json()
+  try {
+    const user = await User.findById(userid)
+    if (!user) {
+      return NextResponse.json(
+        { success: false, message: 'User not found' },
+        { status: 404 }
+      )
+    }
+    user.verficatiionStatus = 'approved'
+    await user.save()
+
+    return NextResponse.json(
+      { success: true, message: 'User verified successfully', user: user },
+      { status: 200 }
+    )
+  } catch (error) {
+    console.error('Error verifying user:', error)
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Error verifying user',
+      },
+      { status: 500 }
+    )
+  }
+}
