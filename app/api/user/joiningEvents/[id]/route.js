9c651f3b73c45cf3a6cd9a9a612a2222eedf6275
HtetAungShine6 <htetaungshine211299@gmail.com>
2025-02-25 17:01:00 +0700

    Fix: Populate Joining Event Route V6

--- app/api/user/joiningEvents/[id]/route.js
+++ app/api/user/joiningEvents/[id]/route.js
@@ -77,15 +77,9 @@ export async function GET(req, { params }) {
 
   try {
     // Fetch events the user has joined and populate participants + organizer in one query
-    const joiningEvents = await Event.find({ 'participants._id': id })
-    .populate({
-      path: 'participants._id',
-      select: 'name profileImageUrl instagramLink verificationStatus bio about'
-    })
-    .populate({
-      path: 'organizer._id',
-      select: 'name profileImageUrl instagramLink verificationStatus bio about'
-    })
+    const joiningEvents = await Event.find({ 'participants': id })
+      .populate('participants._id', 'name profileImageUrl instagramLink verificationStatus bio about')
+      .populate('organizer._id', 'name profileImageUrl instagramLink verificationStatus bio about')
 
     // Format events
     const formattedEvents = joiningEvents.map(event => ({
